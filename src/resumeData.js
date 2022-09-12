3d190a7db979a7cdff500914412aaf6c5d80fe95
Mohamed Hussin <medoalking2016@gmail.com>
2022-09-12 22:16:26 +0200

    website React

---
+++ src/resumeData.js
@@ -0,0 +1,104 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Mohamed Hussin",
+    "role": "Frontend Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "facebook": "Your facebook",
+    "roleDescription": "I am front-end-developer ..designing and developing websites to be easy to use and responsive to suits all screen sizes using the latest web techniques like : HTML5, CSS3, JS, Bootstrap. web users then provide them the best experience which enhances the number of website users, moving among menus of this website and makes user reaches his demand by the least number of clicks.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/muhammad-hussein-shams-4886a120b/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/medoalking2016",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com/profile.php?id=100008284878881",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":"I'm currently online learning, I'm a self-taught Frontend Developer, and I'm digging into my Backend Devloper. I believe that to be successful in life one needs to be obsessed with their dreams and keep working towards them.",
+    "address":"Egypt",
+    "website":"",
+    "education":[
+      
+      {
+        "UniversityName":"Faculty of Arts in Minya",
+        "specialization":"Population Studies",
+        "MonthOfPassing":"NOV",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+  
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS3"
+      },
+      {
+        "skillname":"Es6"
+      },
+      {
+        "skillname":"Bootstrap"
+      },
+      {
+        "skillname":"ReactJs"
+      },
+      {
+        "skillname":"PHP"
+      },
+      {
+        "skillname":"MYSQL"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1 ",
+        "description":"mobileapp",
+        "imgurl":"https://github.com/medoalking2016/Website-Profile/raw/main/Screenshot%202022-09-03%20164149.png",
+        "wbsite":"https://github.com/medoalking2016/Website-Profile"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"https://github.com/medoalking2016/Api-Werther/raw/master/Wether.jpg",
+        "wbsite":"https://github.com/medoalking2016/Api-Werther"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"https://github.com/medoalking2016/website/blob/main/Screenshot%202022-09-12%20210744.png?raw=true",
+        "wbsite":"https://github.com/medoalking2016/website"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"If God releases your tongue in supplication, know that He wants to give you what you want, no matter how great your desire and the greatness of your request.        ",
+        "name":"old sage"
+      },
+      {
+        "description":"You don't have to be great to dream, but you have to dream in order to be great. ",
+        "name":"old sage"
+      },
+      {
+        "description":"You don't have to be great to dream, but you have to dream in order to be great. ",
+        "name":"old sage"
+      }
+    ]
+  }
+  
+  export default resumeData
